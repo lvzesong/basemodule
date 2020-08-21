6cbf59d2d0217c39317906280140b74a4d3d93ce
lzsong <343067508@qq.com>
2020-08-21 10:34:57 +0800

    添加retrofit rxjava3adapter

--- app/src/main/java/com/base/basemodule/di/modules/RetrofitModule.java
+++ app/src/main/java/com/base/basemodule/di/modules/RetrofitModule.java
@@ -29,7 +29,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
@@ -75,7 +75,7 @@ public final class RetrofitModule {
                     //.addConverterFactory(ScalarsConverterFactory.create())
                     .addConverterFactory(BaseApplication.getApplication().getAppConfig().getNetConfig().getJsonConverterFactory() == null ? GsonConverterFactory.create() : BaseApplication.getApplication().getAppConfig().getNetConfig().getJsonConverterFactory())//
                     //.addConverterFactory(GsonConverterFactory.create())
-                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
                     .client(client)
                     .build();
         }
