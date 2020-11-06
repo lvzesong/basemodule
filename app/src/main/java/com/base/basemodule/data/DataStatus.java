110dd08123e776166e47d72389758db16254b686
lzsong <343067508@qq.com>
2020-11-06 11:03:50 +0800

    修改DataStatus

--- app/src/main/java/com/base/basemodule/data/DataStatus.java
+++ app/src/main/java/com/base/basemodule/data/DataStatus.java
@@ -16,53 +16,68 @@ import static com.base.basemodule.data.Status.SUCCESS;
  */
 //a generic class that describes a data with a status
 public class DataStatus<T> {
+    public int code;
     @NonNull
     public final Status status;
     @Nullable
     public final T data;
-    @Nullable public final String message;
-    private DataStatus(@NonNull Status status, @Nullable T data, @Nullable String message) {
+    @Nullable
+    public final String message;
+
+    private DataStatus(@NonNull Status status, @Nullable T data, @Nullable String message, int code) {
         this.status = status;
         this.data = data;
+        this.code = code;
         this.message = message;
     }
 
-       public static <T> DataStatus<T> success(@NonNull T data) {
-        return new DataStatus<>(SUCCESS, data, null);
+    public static <T> DataStatus<T> success(@NonNull T data) {
+        return new DataStatus<>(SUCCESS, data, null, 0);
     }
 
-    public static <T> DataStatus<T> success(@NonNull T data,@NonNull String msg) {
-        return new DataStatus<>(SUCCESS, data, msg);
+    public static <T> DataStatus<T> success(@NonNull T data, @NonNull String msg) {
+        return new DataStatus<>(SUCCESS, data, msg, 0);
+    }
+
+    public static <T> DataStatus<T> success(@NonNull T data, @NonNull String msg, int code) {
+        return new DataStatus<>(SUCCESS, data, msg, code);
     }
 
     public static <T> DataStatus<T> success(@NonNull String msg) {
-        return new DataStatus<>(SUCCESS, null, msg);
+        return new DataStatus<>(SUCCESS, null, msg, 0);
     }
 
+    public static <T> DataStatus<T> success(@NonNull String msg, int code) {
+        return new DataStatus<>(SUCCESS, null, msg, code);
+    }
 
 
     public static <T> DataStatus<T> error(@NonNull T data) {
-        return new DataStatus<>(ERROR, data, null);
+        return new DataStatus<>(ERROR, data, null, 0);
     }
 
-    public static <T> DataStatus<T> error(@NonNull T data,@NonNull String msg) {
-        return new DataStatus<>(ERROR, data, msg);
+    public static <T> DataStatus<T> error(@NonNull T data, @NonNull String msg) {
+        return new DataStatus<>(ERROR, data, msg, 0);
     }
 
     public static <T> DataStatus<T> error(@NonNull String msg) {
-        return new DataStatus<>(ERROR, null, msg);
+        return new DataStatus<>(ERROR, null, msg, 0);
+    }
+
+    public static <T> DataStatus<T> error(@NonNull String msg, int code) {
+        return new DataStatus<>(ERROR, null, msg, code);
     }
 
     public static <T> DataStatus<T> empty(@NonNull T data) {
-        return new DataStatus<>(EMPTY, data, null);
+        return new DataStatus<>(EMPTY, data, null, 0);
     }
 
-    public static <T> DataStatus<T> empty(@NonNull T data,@NonNull String msg) {
-        return new DataStatus<>(EMPTY, data, msg);
+    public static <T> DataStatus<T> empty(@NonNull T data, @NonNull String msg) {
+        return new DataStatus<>(EMPTY, data, msg, 0);
     }
 
     public static <T> DataStatus<T> empty(@NonNull String msg) {
-        return new DataStatus<>(EMPTY, null, msg);
+        return new DataStatus<>(EMPTY, null, msg, 0);
     }
 
 
@@ -70,20 +85,20 @@ public class DataStatus<T> {
 //        return new DataStatus<>(LOADING, data, null);
 //    }
 
-    public static <T> DataStatus<T> loading(@NonNull T data,@NonNull String msg) {
-        return new DataStatus<>(LOADING, data, msg);
+    public static <T> DataStatus<T> loading(@NonNull T data, @NonNull String msg) {
+        return new DataStatus<>(LOADING, data, msg, 0);
     }
 
     public static <T> DataStatus<T> loading(@NonNull String msg) {
-        return new DataStatus<>(LOADING, null, msg);
+        return new DataStatus<>(LOADING, null, msg, 0);
     }
 
-    public static <T> DataStatus<T> hide(@NonNull T data,@NonNull String msg) {
-        return new DataStatus<>(HIDE, data, msg);
+    public static <T> DataStatus<T> hide(@NonNull T data, @NonNull String msg) {
+        return new DataStatus<>(HIDE, data, msg, 0);
     }
 
     public static <T> DataStatus<T> hide(@NonNull String msg) {
-        return new DataStatus<>(HIDE, null, msg);
+        return new DataStatus<>(HIDE, null, msg, 0);
     }
 
 }
